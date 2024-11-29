b3c9666f76484015cd0b6a9f32b229d55c0c13cc
OlhaFedorets <fedoretsolha@gmail.com>
2024-11-29 16:11:23 +0300

    change todo

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,79 +1,97 @@
 import {FilterValuesType, TaskType} from "./App";
-import {ChangeEvent, KeyboardEvent, useState} from "react";
+import {ChangeEvent} from "react";
 import {Button} from "./Button";
+import {AddItemForm} from "./AddItemForm";
+import {EditableSpan} from "./EditableSpan";
 
 type PropsType = {
-	title: string
-	todolistId: string
-	tasks: TaskType[]
-	removeTask: (taskId: string, todolistId: string) => void
-	changeFilter: (filter: FilterValuesType, todolistId: string) => void
-	addTask: (title: string, todolistId: string) => void
-	changeTaskStatus: (taskId: string, taskStatus: boolean, todolistId: string) => void
-	filter: FilterValuesType
-	removeTodolist: (todolistId: string) => void
+    title: string
+    todolistId: string
+    tasks: TaskType[]
+    removeTask: (taskId: string, todolistId: string) => void
+    changeFilter: (filter: FilterValuesType, todolistId: string) => void
+    addTask: (title: string, todolistId: string) => void
+    changeTaskStatus: (taskId: string, taskStatus: boolean, todolistId: string) => void
+    filter: FilterValuesType
+    removeTodolist: (todolistId: string) => void
+    changeTodolistTitle: (title: string, todolistId: string) => void
+    changeTaskTitle: (taskId: string, title: string, todolistId: string) => void
 }
 
 export const Todolist = (props: PropsType) => {
-	const {
-		title,
-		tasks,
-		filter,
-		removeTask,
-		changeFilter,
-		addTask,
-		changeTaskStatus,
-		todolistId,
-		removeTodolist
-	} = props
+    const {
+        title,
+        tasks,
+        filter,
+        removeTask,
+        changeFilter,
+        addTask,
+        changeTaskStatus,
+        todolistId,
+        removeTodolist,
+        changeTodolistTitle,
+        changeTaskTitle,
+    } = props
 
+    const addTaskHandler = (taskTitle: string) => {
+        addTask(taskTitle, todolistId)
+    }
 
+    const changeFilterTasksHandler = (filter: FilterValuesType) => {
+        changeFilter(filter, props.todolistId)
+    }
 
-	const changeFilterTasksHandler = (filter: FilterValuesType) => {
-		changeFilter(filter, props.todolistId)
-	}
+    const removeTodolistHandler = () => {
+        removeTodolist(todolistId)
+    }
 
-	const removeTodolistHandler = () => {
-		removeTodolist(todolistId)
-	}
+    const  changeTodolistTitleHandler = (title: string) => {
+        changeTodolistTitle(title, todolistId)
+    }
 
-	return (
-		<div>
-			<div className={"todolist-title-container"}>
-				<h3>{title}</h3>
-				<Button title={'x'} onClick={removeTodolistHandler}/>
-			</div>
-			{
-				tasks.length === 0
-					? <p>Тасок нет</p>
-					: <ul>
-						{tasks.map((task) => {
+    return (
+        <div>
+            <div className={"todolist-title-container"}>
+                <h3>
+                    <EditableSpan title={title} changeTitle={changeTodolistTitleHandler}/>
+                </h3>
+                <Button title={'x'} onClick={removeTodolistHandler}/>
+            </div>
+            <AddItemForm addItem={addTaskHandler}/>
+            {
+                tasks.length === 0
+                    ? <p>Тасок нет</p>
+                    : <ul>
+                        {tasks.map((task) => {
 
-							const removeTaskHandler = () => {
-								removeTask(task.id, todolistId)
-							}
+                            const removeTaskHandler = () => {
+                                removeTask(task.id, todolistId)
+                            }
+                            const changeTaskTitleHandler = (title: string) => {
+                                changeTaskTitle(task.id, title, todolistId)
+                            }
 
-							const changeTaskStatusHandler = (e: ChangeEvent<HTMLInputElement>) => {
-								const newStatusValue = e.currentTarget.checked
-								changeTaskStatus(task.id, newStatusValue, todolistId)
-							}
+                            const changeTaskStatusHandler = (e: ChangeEvent<HTMLInputElement>) => {
+                                const newStatusValue = e.currentTarget.checked
+                                changeTaskStatus(task.id, newStatusValue, todolistId)
+                            }
 
-							return <li key={task.id} className={task.isDone ? 'is-done' : ''}>
-								<input type="checkbox" checked={task.isDone} onChange={changeTaskStatusHandler}/>
-								<span>{task.title}</span>
-								<Button onClick={removeTaskHandler} title={'x'}/>
-							</li>
-						})}
-					</ul>
-			}
-			<div>
-				<Button className={filter === 'all' ? 'active-filter' : ''} title={'All'}
-				        onClick={() => changeFilterTasksHandler('all')}/>
-				<Button className={filter === 'active' ? 'active-filter' : ''} title={'Active'}
-				        onClick={() => changeFilterTasksHandler('active')}/>
-				<Button className={filter === 'completed' ? 'active-filter' : ''} title={'Completed'}
-				        onClick={() => changeFilterTasksHandler('completed')}/>
-			</div>
-		</div>
-	)
+                            return <li key={task.id} className={task.isDone ? 'is-done' : ''}>
+                                <input type="checkbox" checked={task.isDone} onChange={changeTaskStatusHandler}/>
+                                <EditableSpan changeTitle={changeTaskTitleHandler} title={task.title}/>
+                                <Button onClick={removeTaskHandler} title={'x'}/>
+                            </li>
+                        })}
+                    </ul>
+            }
+            <div>
+                <Button className={filter === 'all' ? 'active-filter' : ''} title={'All'}
+                        onClick={() => changeFilterTasksHandler('all')}/>
+                <Button className={filter === 'active' ? 'active-filter' : ''} title={'Active'}
+                        onClick={() => changeFilterTasksHandler('active')}/>
+                <Button className={filter === 'completed' ? 'active-filter' : ''} title={'Completed'}
+                        onClick={() => changeFilterTasksHandler('completed')}/>
+            </div>
+        </div>
+    )
 }
