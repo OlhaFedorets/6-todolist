6d73e99e185d52f7eabe72e690578ea51e820a44
OlhaFedorets <fedoretsolha@gmail.com>
2024-11-29 09:58:23 +0300

    add addItemForm

---
+++ src/AddItemForm.tsx
@@ -0,0 +1,45 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {Button} from "./Button";
+
+
+type AddItemFormPropsType = {
+    addItem: (title: string) => void
+}
+
+export const AddItemForm = ({addItem}: AddItemFormPropsType) => {
+    const [itemTitle, setItemTitle] = useState('')
+    const [error, setError] = useState<string | null>(null)
+
+    const changeItemTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setItemTitle(event.currentTarget.value)
+    }
+
+    const addItemOnKeyUpHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        setError(null)
+        if (event.key === 'Enter') {
+            addItemHandler()
+        }
+    }
+
+    const addItemHandler = () => {
+        if (itemTitle.trim() !== '') {
+            addItem(itemTitle.trim())
+            setItemTitle('')
+        } else {
+            setError('Title is required')
+        }
+    }
+
+    return (
+        <div>
+            <input
+                className={error ? 'error' : ''}
+                value={itemTitle}
+                onChange={changeItemTitleHandler}
+                onKeyUp={addItemOnKeyUpHandler}
+            />
+            <Button title={'+'} onClick={addItemHandler}/>
+            {error && <div className={'error-message'}>{error}</div>}
+        </div>
+    );
+};
